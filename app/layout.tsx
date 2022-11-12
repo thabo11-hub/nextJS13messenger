871a9ee80f73a6244eff858dec6a2d23c9892149
Thabo Sodi <thabosodi11@gmail.com>
2022-11-13 01:19:34 +0200

    get unstable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import { unstable_getServerSession } from "next-auth";
 import "../styles/globals.css";
 import Header from "./Header";
 import { Providers } from "./providers";
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const session = await unstable_getServerSession();
     return (
         <html>
             <head />
