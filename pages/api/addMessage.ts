e6633536dc22f8084c6d4eaf8c8ae26746ee6950
Thabo Sodi <thabosodi11@gmail.com>
2022-11-12 09:10:35 +0200

    push messages to redis

--- pages/api/addMessage.ts
+++ pages/api/addMessage.ts
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import redis from "../../redis"
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -20,6 +21,9 @@ export default function handler(
     ...message,
     created_at: Date.now(),
   };
+
+  //push to redis
+  await redis.hset('message', message.id, JSON.stringify(newMessage));
 
   res.status(200).json({ name: "John Doe" });
 }
