25611db2f515ba1f95ccce41b3da0782893177e7
Thabo Sodi <thabosodi11@gmail.com>
2022-11-13 01:01:59 +0200

    sign in oky

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
 type Props = {
     providers: Awaited<ReturnType<typeof getProviders>>;
@@ -8,15 +8,18 @@ type Props = {
 function SignInComponent({ providers }: Props) {
     return (
         <div>
-            {Object.values(providers!).map((provider) => {
+            {Object.values(providers!).map((provider) => (
                 <div key={provider.name}>
-                    <button>
+                    <button onClick={() => signIn(provider.id, {
+                        callbackUrl: process.env.VERCEL_URL || "http://localhost:3000"
+                    })}>
                         sign in with {provider.name}
                     </button>
                 </div>
-            })}
-        </div>
-    )
+            ))
+            }
+        </div >
+    );
 }
 
 export default SignInComponent;
