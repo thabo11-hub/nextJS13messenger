bd0fa98d07c65e19fd91fd4ccb330859e4472b30
Thabo Sodi <thabosodi11@gmail.com>
2022-11-12 09:01:26 +0200

    addMesageApi

---
+++ pages/api/addMessage.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+    if (req.method !== 'POST') {
+        res.status(405).json({name:'Method Not Allowed'})
+        return
+    }
+
+  res.status(200).json({ name: 'John Doe' })
+}
