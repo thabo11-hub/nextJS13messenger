2c71ddf55636eac5baf4ea1e6c69092ef6c52a39
Thabo Sodi <thabosodi11@gmail.com>
2022-11-12 13:47:23 +0200

    message Component css

--- app/MessageComponent.tsx
+++ app/MessageComponent.tsx
@@ -19,13 +19,13 @@ function MessageComponent({ message }: Props) {
                 />
             </div>
             <div>
-                <p className="text-[0.65rem] px-[2px] pb-[2px]">{message.username}</p>
-                <div>
-
-                    <div>
+                <p className="text-[0.65rem] px-[2px] pb-[2px] text-red-400">{message.username}</p>
+                
+                <div className="flex items-end">
+                    <div className="px-3 py-2 rounded-lg w-fit text-white bg-rose-400">
                         <p>{message.message}</p>
                     </div>
-                    <p>{new Date(message.created_at).toLocaleString()}</p>
+                    <p className="text-[0.65rem] italic px-2 text-gray-300">{new Date(message.created_at).toLocaleString()}</p>
                 </div>
             </div>
         </div>
