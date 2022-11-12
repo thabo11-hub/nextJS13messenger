de1c25a6ef84d86af9e8aa7f6e441d0404a61931
Thabo Sodi <thabosodi11@gmail.com>
2022-11-12 09:56:22 +0200

    sort message

---
+++ pages/api/getMessages.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import redis from "../../redis";
+import { Message } from "../../typings";
+
+type Data = {
+  message: Message;
+};
+
+type ErrorData = {
+  body: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorData>
+) {
+  if (req.method !== "GET") {
+    res.status(405).json({ body: "Method Not Allowed" });
+    return;
+  }
+
+  const messageRes = await redis.hvals("message");
+
+  //pass the response
+  const messags: Message[] = messageRes
+    .map((message) => JSON.parse(message))
+    .sort((a, b) => b.created_at - a.created_at);
+
+  res.status(200).json({ message: newMessage });
+}
