e9b58a35dbbafd6a0daf6801f3e9356fd0b6e839
Thabo Sodi <thabosodi11@gmail.com>
2022-11-13 10:35:17 +0200

    session on name and image and block return

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -22,7 +22,7 @@ function ChatInput({ session }: Props) {
   const addMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!input) return;
+    if (!input || !session) return;
 
     const messageToSend = input;
 
@@ -34,9 +34,9 @@ function ChatInput({ session }: Props) {
       id,
       message: messageToSend,
       created_at: Date.now(),
-      username: "Eldridge",
-      profilePic: "https://imgs.search.brave.com/I89N79oEi5Im8M-4hQmtO5NepNLtXksKsD1Pu4dM71I/rs:fit:256:256:1/g:ce/aHR0cHM6Ly9yZXMt/MS5jbG91ZGluYXJ5/LmNvbS9jcnVuY2hi/YXNlLXByb2R1Y3Rp/b24vaW1hZ2UvdXBs/b2FkL2NfbHBhZCxo/XzI1Nix3XzI1Nixm/X2F1dG8scV9hdXRv/OmVjby9rYWx6ZGdw/cmFvZW1vaHBvcXFm/Zg",
-      email: "thabosodi11@gmail.com"
+      username: session?.user?.name!,
+      profilePic: session?.user?.image!,
+      email: session?.user?.email!
     };
 
     const uploadMessageToUpstash = async () => {
