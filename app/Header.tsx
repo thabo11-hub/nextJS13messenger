8a4885d578762a0c238b42cb336969a5a181816b
Thabo Sodi <thabosodi11@gmail.com>
2022-11-13 10:24:40 +0200

    get session state

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import LogoutButton from "./LogoutButton";
+import { unstable_getServerSession } from "next-auth";
 
-function Header() {
-    const session = true;
+async function Header() {
+    const session = await unstable_getServerSession();
 
     if (session) return (
         <header className="sticky top-0 z-50 bg-sky-100 flex justify-between items-center p-10 shadow-sm">
