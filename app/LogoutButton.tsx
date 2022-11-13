29bf587580462e14fbeb5fa93354df4db184d2b9
Thabo Sodi <thabosodi11@gmail.com>
2022-11-13 10:23:05 +0200

    sign out function

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { signOut } from "next-auth/react";
+
 function LogoutButton() {
     return (
         <button
-            onClick={() => console.log("hello!")}
+            onClick={() => signOut()}
             className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
             Logout
         </button>
